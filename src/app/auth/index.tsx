881b28fb945df586a426ab459ef6cdcaa3135127
marksikaundi <emmanuelmark117@gmail.com>
2023-05-27 22:45:12 +0200

    working on components

---
+++ src/app/auth/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type AuthPageProps = {};
+
+const AuthPage: React.FC<AuthPageProps> = () => {
+  return <div>index</div>;
+};
+
+export default AuthPage;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = () => {
+    return (
+        <div>
+        Navbar
+        </div>
+    )
+}
+export default Navbar;
